0996d600ada8ac9fbeec6b3eab627fcdcc373d19
jhonnymalta <jhonatamalta@hotmail.com>
2024-06-27 17:23:21 -0300

    criado setHangarDto

---
+++ Emissora_Tv_Api/DTOs/SetHangar.cs
@@ -0,0 +1,10 @@
+﻿namespace Emissora_Tv_Api.DTOs
+{
+    public class SetHangar
+    {
+        public int Id { get; set; }
+        public  string Nome { get; set; }
+        public double TamanhoM2 { get; set; }
+        public int Capacidade { get; set; }
+    }
+}
